1544ec99f611fe600d52add094c9e0cbe80d306e
Bryan Stitt <bryan@stitthappens.com>
2016-12-10 00:06:07 -0800

    more logs

--- bin/udev.sh
+++ bin/udev.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 DVD_RIP_BIN_DIR="$(cd "$(dirname "$0")" && pwd -P)"
+
+echo "====" >>/var/log/dvd-rip.env
+uptime >>/var/log/dvd-rip.env
+pwd >>/var/log/dvd-rip.env
+env >>/var/log/dvd-rip.env
+echo "args: $*" >>/var/log/dvd-rip.env
+
 echo "$DVD_RIP_BIN_DIR/dvd-to-vob.sh" "$@" | at now
