1ad8bed0fbc1af2705c7bbeb2337ec01de5fefac
Bryan Stitt <bryan@stitthappens.com>
2016-12-10 00:23:18 -0800

    i guess grep doesn't have -c

--- bin/vob-to-handbrake.sh
+++ bin/vob-to-handbrake.sh
@@ -17,7 +17,7 @@ transcode() {
 
     local all_the_tracks=1,2,3,4,5,6,7,8,9,10
 
-    if [ "$(find "$src_dir" -name "*.vob" -c)" -gt 1 ]; then
+    if [ "$(find "$src_dir" -name "*.vob" -print | wc -l)" -gt 1 ]; then
         echo "Multple vobs found. Combining them now..."
         cat_and_cleanup "$src_dir/combined.vob" "$src_dir"/*.vob
     fi
